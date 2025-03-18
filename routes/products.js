3b9f5ff8d13c2b0458fa4e8335ebf61052900b4b
Alejo Florentin <alejoflorentin@gmail.com>
2025-03-18 17:33:41 -0300

    feat:create edit and create porducts page

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,17 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-let productsController = require("../controllers/productsController");
+let productsController = require('../controllers/productsController');
 
 /* GET users listing. */
-router.get("/", productsController.productos);
+router.get('/', productsController.productos);
 
-router.get("/carrito", productsController.carrito);
+router.get('/carrito', productsController.carrito);
 
-router.get("/detalle/:id", productsController.detalle);
+router.get('/detalle/:id', productsController.detalle);
+
+router.get('/crear', productsController.crear);
+
+router.get('/editar', productsController.editar);
 
 module.exports = router;
