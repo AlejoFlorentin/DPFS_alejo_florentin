58c655c6fad8e27c7573cff487abc8509c320a81
Alejo Florentin <alejoflorentin@gmail.com>
2025-07-17 20:46:23 -0300

    style: update style.css

--- public/js/products.js
+++ public/js/products.js
@@ -1,50 +1,43 @@
 // Agregar eventos de clic a los botones de "Agregar al carrito"
-document.addEventListener('DOMContentLoaded', () => {
-  let btnAddProduct = document.querySelectorAll('.btnAddProduct');
-  const btnFilter   = document.getElementById('filterBtn');
-  const filterMenu  = document.getElementById('filter');
+document.addEventListener("DOMContentLoaded", () => {
+  let btnAddProduct = document.querySelectorAll(".btnAddProduct");
+  const btnFilter = document.getElementById("filter-btn");
+  const filterMenu = document.getElementById("filter");
 
-btnFilter.addEventListener('click', () => {
-  console.log('Hola');
-  // Alterna inline-style
-  filterMenu.style.display = 
-    filterMenu.style.display === 'block' ? 'none' : 'block';
-});
-
-  btnAddProduct.forEach(button => {
-    button.addEventListener('click', addProduct);
+  btnAddProduct.forEach((button) => {
+    button.addEventListener("click", addProduct);
   });
 
   function addProduct(e) {
     Toastify({
-      text: 'Producto Agregado',
+      text: "Producto Agregado",
       duration: 2000,
-      gravity: 'top',
-      position: 'right',
+      gravity: "top",
+      position: "right",
       offset: {
         x: 20,
         y: 150,
       },
       stopOnFocus: true,
       style: {
-        background: 'black',
+        background: "black",
       },
     }).showToast();
   }
 
-  document.querySelectorAll('.formEliminarProducto').forEach(form => {
-    form.addEventListener('submit', function (e) {
+  document.querySelectorAll(".formEliminarProducto").forEach((form) => {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       Swal.fire({
-        title: '¿Estás seguro?',
-        text: '¡Esta acción eliminará el producto!',
-        icon: 'warning',
+        title: "¿Estás seguro?",
+        text: "¡Esta acción eliminará el producto!",
+        icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
-        confirmButtonText: 'Sí, eliminar',
-        cancelButtonText: 'Cancelar',
-      }).then(result => {
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
         if (result.isConfirmed) {
           form.submit();
         }
--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-require('dotenv').config();
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
+require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
 
 // Asegúrate de que la ruta sea correcta
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const productsRouter = require('./routes/products');
-const cartRouter = require('./routes/cart');
-const methodOverride = require('method-override');
-const sessionMiddleware = require('./middlewares/sessionMiddleware');
-const cartMiddleware = require('./middlewares/cartMiddleware');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const productsRouter = require("./routes/products");
+const cartRouter = require("./routes/cart");
+const methodOverride = require("method-override");
+const sessionMiddleware = require("./middlewares/sessionMiddleware");
+const cartMiddleware = require("./middlewares/cartMiddleware");
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/data', express.static(path.join(__dirname, 'data')));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("/data", express.static(path.join(__dirname, "data")));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(methodOverride('_method')); // para poder usar el put y el delete en los formularios
+app.use(logger("dev"));
+app.use(methodOverride("_method")); // para poder usar el put y el delete en los formularios
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(
   session({
-    secret: 'superlative',
+    secret: "superlative",
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -43,10 +43,10 @@ app.use(
 app.use(sessionMiddleware);
 app.use(cartMiddleware);
 
-app.use('/', indexRouter);
-app.use('/usuarios', usersRouter);
-app.use('/productos', productsRouter);
-app.use('/carrito', cartRouter);
+app.use("/", indexRouter);
+app.use("/usuarios", usersRouter);
+app.use("/productos", productsRouter);
+app.use("/carrito", cartRouter);
 
 const port = process.env.PORT || 3000;
 
